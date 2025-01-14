4a3b7b79e39985b2335305af7a33d5c1a0bd539b
dh0728 <dustkscjswo@naver.com>
2025-01-14 17:53:32 +0900

    0114codeadd

---
+++ books/ch05/data-jpa/src/main/java/me/song/data_jpa/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+//package me.song.data_jpa.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class AddUserRequest {
+//    private String email;
+//    private String password;
+//}
