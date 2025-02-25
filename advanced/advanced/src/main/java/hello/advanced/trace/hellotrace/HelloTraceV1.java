5e7a6533fb60dec6a4701cddcb9a239cf905894a
dh0728 <dustkscjswo@naver.com>
2025-02-26 01:04:55 +0900

    advanced

---
+++ advanced/advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
@@ -0,0 +1,11 @@
+package hello.advanced.trace.hellotrace;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV1 {
+    
+}
